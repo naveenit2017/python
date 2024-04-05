3101bc065bbbfffb2cf44f896f47c0269a5b5a86
naveenit2017 <114174494+naveenit2017@users.noreply.github.com>
2024-04-05 05:31:38 +0000

    aaa

---
+++ my_variables.py
@@ -0,0 +1,15 @@
+# python variables
+user_name = "naveen varma"
+total_iteams = 27
+print(f"{user_name} and {total_iteams}")
+
+#Define the configuration variables for server
+server_name = "my_server"
+port = 80
+max_connects = 1000
+is_http_enabled = True
+
+print(f"server_name :" ,server_name)
+print(f"total_iteams :",total_iteams)
+print(f"max_connects :{max_connects}")
+print(f"http_port :{port}")
