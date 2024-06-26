2b8e99462c56bac234635668e1a12f04e03b0672
naveenit2017 <114174494+naveenit2017@users.noreply.github.com>
2024-06-26 07:44:31 +0530

    Update Exp_Handling.py
    Signed-off-by: naveenit2017 <114174494+naveenit2017@users.noreply.github.com>

--- examples/Exp_Handling.py
+++ examples/Exp_Handling.py
@@ -18,3 +18,22 @@ else:
     print("There is no error")
 finally:
     print("it is always executed ")
+
+------------
+#Exception handling
+
+#try --will keep the risk code
+#except --if try will get error it will execute
+#else --no error
+#finally:
+
+try:
+    a=10%0
+    print(a)
+except:
+    print("Try block have issue please check")
+else:
+    print("Time god no error")
+finally:
+    print("It will execute always")
+
