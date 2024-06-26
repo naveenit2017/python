ff6f0d5ea8bae593f0309097db2971d6fcac1b30
naveenit2017 <114174494+naveenit2017@users.noreply.github.com>
2024-06-26 07:25:35 +0530

    Update Class_Example.py
    Signed-off-by: naveenit2017 <114174494+naveenit2017@users.noreply.github.com>

--- examples/Class_Example.py
+++ examples/Class_Example.py
@@ -1,13 +1,27 @@
-class MyClass:
-    print("This is Naveen from US")
-    a=10
-    print(a)
-    def wish(self,x):
-        print("happy birth day anna")
-        #print(x)
-    def greetings(self,z):
-        print("Hello goodddd morning")
-       # print(z)
-mc=MyClass()
-mc.wish(10)
-mc.greetings(30)
+#clss example
+class myclass:
+    def __init__(self,name,age) -> None:
+        print(f"my name is{name} and age is {age}")
+        
+    print("I am from Myclass")
+    def greetings(self):
+        print("Hello this is naveen varma")
+    def welecome(self,name):
+        print(f"Thanks welcome mr {name} varma garu")
+
+m=myclass("naveen",32)    
+m.greetings()
+m.welecome("naveen")
+
+#__init__ is the initializer method that sets up the instance attributes name and age.
+#self.name and self.age refer to the name and age attributes of the instance being created.
+
+class myclass:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def greetings(self):
+        return f"He is my best friend is name {self.name} and age is {self.age}"
+    
+m=myclass("Raja",25)
+print(m.greetings())
