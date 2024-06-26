bc34854620d0cb38d4e878007d9a88f331ab5cde
naveenit2017 <114174494+naveenit2017@users.noreply.github.com>
2024-06-26 07:02:45 +0530

    Update ArmStronng.py
    Signed-off-by: naveenit2017 <114174494+naveenit2017@users.noreply.github.com>

--- examples/ArmStronng.py
+++ examples/ArmStronng.py
@@ -12,3 +12,29 @@ for i in range(1000):
         i=i//10
     if res==num:
         print(res)
+
+=============================
+#Armstrong number
+#logic--
+#1.Take the input number
+#2.length of that number
+#3.power of lenth ,that individual number.
+
+nu=int(input("Enter the number"))
+num1=nu
+n=len(str(num1))
+print(n)
+sum=0
+
+print(type(num1))
+for num in range(num1):
+    while num>0:
+        print(sum)
+        d=num%10
+        sum=sum+d**n
+        num=num//10
+    if sum==nu:
+        print("Armstrong number")
+        print(sum)
+    else:
+        print("It is not amrmstrong number")
